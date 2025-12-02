83b82740335ff31a9c16ea604d9ae06f9d4c53fb
baguiostrawberryjam <johnuelleangel@gmail.com>
2025-12-02 10:45:23 +0800

    Centered Hello page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="w-full min-h-screen flex justify-center items-center">
       Hello, Bulsu! Hi!
     </div>
   );
