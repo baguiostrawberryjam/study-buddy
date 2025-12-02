460ef9abcf977f72f2b03dd07dce21d9b5ad3f54
baguiostrawberryjam <johnuelleangel@gmail.com>
2025-12-02 11:39:36 +0800

    Home to Login routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen flex justify-center items-center">
-      Hello, Bulsu! Hi!
-    </div>
+      <div className="w-full min-h-screen flex justify-center items-center">
+        
+        <div className="w-full min-h-screen flex">
+
+          <h1>ChatGPT</h1>
+
+          <div>
+            <button type="button" className="text-white bg-brand box-border border border-gray hover:bg-brand-strong rounded-full focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-1.5 focus:outline-none">
+              <Link href="/login">Login</Link>
+            </button>
+          </div>
+
+        </div>
+
+      </div>
   );
 }
